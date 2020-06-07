f667197d3dab9789311510b5b0f38b87cd010e07
Alan Novaes <alanovaes@live.com>
2020-06-07 08:56:31 -0300

    initial commit

---
+++ public/scripts/create-point.js
@@ -0,0 +1,94 @@
+
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/")
+    .then((res) => {return res.json()} )
+    .then(states => {
+        for(const state of states){
+
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+
+        }
+        
+    })
+}
+
+populateUFs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexofSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexofSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+    
+    fetch(url)
+    .then(res => res.json() )
+    .then(cities => {
+        for(const city of cities) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+        
+        citySelect.disabled = false
+    })
+
+}
+
+
+
+
+
+
+
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+
+// itens de coleta
+// pegar todos os LIs
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+// add/remove class with jscript
+    const itemLi = event.target
+
+    itemLi.classList.toggle("selected")  //selection switch
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId //booleano
+        return itemFound
+    })
+
+    if(alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+    collectedItems.value = selectedItems
+}
---
+++ src/database/db.js
@@ -0,0 +1,84 @@
+//importar a dependencia do sqlite3
+const sqlite3 = require("sqlite3").verbose()
+
+//iniciar o obj de banco de dados
+const db = new sqlite3.Database("./src/database/database.db")
+
+module.exports = db
+
+//utilizar o obj do banco de dados
+
+db.serialize(() => {
+    //acoes com comandos SQL
+    //criar uma tabela
+    // db.run(`
+    //     CREATE TABLE IF NOT EXISTS places (
+    //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         image TEXT,
+    //         name TEXT,
+    //         address TEXT,
+    //         address2 TEXT,
+    //         state TEXT,
+    //         city TEXT,
+    //         items TEXT
+    //     );
+    // `)
+    // //inserir dados na tabela
+    // const query = `
+    //     INSERT INTO places (
+    //         image,
+    //         name,
+    //         address,
+    //         address2,
+    //         state,
+    //         city,
+    //         items
+    //     ) VALUES (?,?,?,?,?,?,?);
+    // `
+    
+    // const values = [
+    //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    //     "Papersider",
+    //     "Guilherme Gemballa, Jardim América",
+    //     "Nº 260",
+    //     "Santa Catarina",
+    //     "Rio do Sul",
+    //     "Papéis e papelão"
+    // ]
+    
+    // function afterInsertData(err) {
+    //     if (err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Cadastrado com sucesso")
+    //     console.log(this)
+    // }
+    
+    // db.run(query, values, afterInsertData)
+
+    //consultar dados da tabela
+
+//    db.all(`SELECT name FROM places`, function(err, rows) {
+//        if (err) {
+//            return console.log(err)
+//        }
+
+//        console.log("Aqui estao seus registros")
+//        console.log(rows)
+        
+        
+//    })
+
+    //deletar um dado da tabela
+    
+//    db.run(`DELETE FROM places WHERE id = ?`, [4], function(err, rows){
+//        if (err) {
+//             return console.log(err)
+//         }
+
+//         console.log("Registro deletado com sucesso")
+//         console.log(rows)
+//     })
+
+})
